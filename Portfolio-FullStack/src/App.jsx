31269de869bfdfa072264b5461d7d0a657f1acfd
sebastian <sebastian.navarropiedra@ucreativa.com>
2026-05-08 19:31:59 -0600

    ..
    ..

--- Portfolio-FullStack/src/App.jsx
+++ Portfolio-FullStack/src/App.jsx
@@ -1,10 +1,11 @@
 
 import './Styles/App.css'
-import Home from './Pages/Home'
-import Aboutme from './Pages/Aboutme'
-import Projects from './Pages/Projects'
-import Contact from './Pages/Contact'
-import NotFound from './Pages/404'
+import Home from './Pages/Home.jsx'
+import Aboutme from './Pages/Aboutme.jsx'
+import Projects from './Pages/Projects.jsx'
+import Contact from './Pages/Contact.jsx'
+import NotFound from './Pages/404.jsx'
+import Navbar from './Components/Navbar.jsx'
 import { BrowserRouter,Routes, Route, Link } from 'react-router-dom'
 
 function App() {
@@ -14,11 +15,14 @@ function App() {
     
 <BrowserRouter> 
 
+<Navbar />
+
   <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Aboutme />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
 </BrowserRouter>
